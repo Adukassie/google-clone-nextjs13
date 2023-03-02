8e1672a1fb39342168d28ab7733f497e1c853089
Adukassie <adugnadagnaw01@gmail.com>
2023-03-02 13:54:01 -0600

    add home header component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,25 @@
-import React from "react";
-export default function page() {
-  return <div className="text-red-600 text-3xl">Home</div>;
+ import HomeHeader from "@/components/HomeHeader";
+// import HomeSearch from "@/components/HomeSearch";
+ import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      {/* Header */}
+      <HomeHeader />
+
+      {/* body */}
+
+      <div className="flex flex-col items-center mt-24">
+        {/* <Image
+          width="300"
+          height="100"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png" alt="logo"
+        /> */}
+
+        {/* <HomeSearch/> */}
+      </div>
+    </>
+  );
 }
+
